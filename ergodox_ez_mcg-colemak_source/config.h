61572dc888cc07443b9d9e0b583c6436cfa0e9a1
Vanessa McGonigle <vanessa_mcgonigle@outlook.com>
2020-04-24 22:26:08 -0400

    Initial Source Control

---
+++ ergodox_ez_mcg-colemak_source/config.h
@@ -0,0 +1,14 @@
+/*
+  Set any config.h overrides for your specific keymap here.
+  See config.h options at https://docs.qmk.fm/#/config_options?id=the-configh-file
+*/
+#define ORYX_CONFIGURATOR
+#undef TAPPING_TERM
+#define TAPPING_TERM 180
+
+#define ONESHOT_TAP_TOGGLE 2
+
+#undef MOUSEKEY_TIME_TO_MAX
+#define MOUSEKEY_TIME_TO_MAX 30
+
+#define FIRMWARE_VERSION u8"VyZgY/daV05"
